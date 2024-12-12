65f58e1e5f0b3aed73a8cc25a61fa282cd9c0c0e
BugHunter-X <askzenai@gmail.com>
2024-12-12 22:56:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+// Correct usage of $inc operator within a findAndUpdate operation
+db.collection('myCollection').findOneAndUpdate({
+  _id: ObjectId("6515f6867b2145546546546")
+}, {
+  $inc: {
+    count: 1 //Should be count: 1 instead of count: '1'
+  }
+});
+```
